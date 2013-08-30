2ca9d0ddc4b845b8364da674806da3d8c0701379
azadov <BarselonA12>
2013-08-30 14:51:18 +0200

    outsource plot creation functions to plot.js

---
+++ js/plot.js
@@ -0,0 +1,175 @@
+// put page numbers at appropriate times on the score time axis
+function createPageTicks(_svg, _pageTimes) {
+    'use strict';
+
+    var key, betterLabelShift;
+    for (key in _pageTimes) {
+        //console.log("page: " + key);
+        if (_pageTimes.hasOwnProperty(key)) {
+            betterLabelShift = (0 - - key) > 9 ? GLVARS.labelShift : GLVARS.labelShift / 2;
+            _svg.append("text")
+                .attr("x", GLVARS.x_scale(_pageTimes[key]) - betterLabelShift)
+                .attr("y", GLVARS.y_scale(0))
+                .attr("font-family", "sans-serif")
+                .attr("font-size", "12px")
+                .attr("fill", "grey")
+                .text(key)
+            ;
+        }
+    }
+}
+
+function createRectangles(_svg, _rects) {
+    'use strict';
+
+    _svg.selectAll(".bar")
+        .data(_rects)
+        .enter().append("rect")
+        .attr("class", "rectangle")
+        .attr("x", function (d) { return GLVARS.x_scale(d.x1); })
+        .attr("width", function (d) { return GLVARS.x_scale(d.width); })
+        .attr("y", function (d) { return GLVARS.y_scale(d.y); })
+        .attr("height", GLVARS.plot_height - GLVARS.y_scale(CONSTANTS.SEGMENT_RECT_HEIGHT))
+        //.on("click", updateVideoPositionRect)
+        .on("mouseover", enlargeVideoDivRect)
+        //.on("mouseout", resetVideoDivRect)
+        .append("svg:title")
+        .text(function (d) {return GLVARS.videoTitle[d.videoID]});
+    ;
+}
+
+function createCurves(_svg, _curves) {
+    'use strict';
+
+    // http://www.dashingd3js.com/svg-paths-and-d3js
+    // var lineData = [ { "x": 61,  "y": 0.75}, { "x": 80,  "y": 0.75},
+    //                  { "x": 55,  "y": 2.75}, { "x": 61,  "y": 2.75}];
+    var lineFunction = d3.svg.line()
+        .x(function (d) { return GLVARS.x_scale(d.x); })
+        .y(function (d) { return GLVARS.y_scale(d.y); })
+        .interpolate("basis");
+
+    _svg.selectAll(".curve")
+        .data(_curves)
+        .enter().append("path")
+        .attr("d", function (d) {return lineFunction(d.points); })
+        .attr("stroke", "blue")
+        .attr("stroke-width", 3)
+        .attr("stroke-dasharray", function (d) {return d.strokeDash; })
+        //.attr("stroke-dasharray", "0,0")
+        .attr("fill", "none")
+        .on("click", updateVideoPositionCurve)
+        .on("mouseover", enlargeVideoDivCurve)
+    //.on("mouseout", resetVideoDivCurve)
+    //;
+}
+
+function createRadioButtons(_svg, _radiobuttons) {
+    'use strict';
+
+    var i, plotDiv = document.getElementById("plotContainer"), selectVideoRB, topForRB, rbID;
+    // if loop starts from 0 then rb will be added from bottom to top and this affects control from keyboard
+    for (i = _radiobuttons.length - 1; i >= 0; i = i - 1) {
+
+        selectVideoRB = document.createElement('input');
+
+        rbID = _radiobuttons[i].videoID + "_" + _radiobuttons[i].segmentIndex + "_RB";
+        selectVideoRB.setAttribute('id', rbID);
+        //selectVideoRB.setAttribute('index', _radiobuttons[i].index);
+        selectVideoRB.setAttribute('type', 'radio');
+        selectVideoRB.setAttribute('name', 'selectVideoGroupRB');
+        selectVideoRB.setAttribute('class', 'videoRB');
+        selectVideoRB.setAttribute('onclick', 'rbClickHandler(this)');
+
+        topForRB = GLVARS.y_scale(_radiobuttons[i].y) + GLVARS.plot_margin.top + GLVARS.plot_margin.bottom;
+        selectVideoRB.style.top = topForRB + "px";
+
+        plotDiv.appendChild(selectVideoRB);
+
+        if (!GLVARS.rbIndex.hasOwnProperty((rbID))) {
+            GLVARS.rbIndex[rbID] = i;
+        }
+
+//        d3.select("g").append("line")
+//            .attr("class", "videoTopLine")
+//            .attr("x1", GLVARS.x_scale(0))
+//            .attr("y1", GLVARS.y_scale(_radiobuttons[i].y))
+//            .attr("x2", GLVARS.x_scale(GLVARS.maxPlotX))
+//            .attr("y2", GLVARS.y_scale(_radiobuttons[i].y))
+//            .attr("stroke-width", 1)
+//            .attr("stroke", "lightgrey")
+//            .attr("pointer-events", "none");
+    }
+
+}
+
+function drawYAxis(svg_basis) {
+    'use strict';
+
+    var yAxis = d3.svg.axis()
+        .scale(GLVARS.y_scale)
+        .orient("left");
+    svg_basis.append("g")
+        .attr("class", "y axis")
+        .call(yAxis)
+        .append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 6)
+        .attr("dy", ".71em")
+        .style("text-anchor", "end")
+        .text("Frequency");
+}
+
+function createPlotSVG() {
+    'use strict';
+
+    var svg_basis = d3.select("#plotContainer").append("svg")
+            .attr("width", GLVARS.plot_width + GLVARS.plot_margin.left + GLVARS.plot_margin.right)
+            .attr("height", GLVARS.plot_height + GLVARS.plot_margin.top + GLVARS.plot_margin.bottom)
+            .append("g")
+            .attr("transform", "translate(" + GLVARS.plot_margin.left + "," + GLVARS.plot_margin.top + ")")
+
+            .on("click", updateScorePosition)
+            .on("mousemove", updateMouseTrackLine)
+            //.on("mousemove", showSuitableVideoDivsForCurrentMousePosition)
+            //.on("mousemove", handleMouseMoveEvent)
+            .on("mouseout", removeMouseTrackLine)
+        ;
+
+    svg_basis.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + GLVARS.plot_height + ")")
+        .call(GLVARS.xAxis);
+
+    // don't show the ticks of x-axis
+    GLVARS.xAxis.tickFormat(function (d) { return ''; });
+
+    //drawYAxis(svg_basis);
+
+    return svg_basis;
+}
+
+function drawPlot(_svg) {
+    'use strict';
+
+    // add blank rectangle
+    _svg.append("rect")
+        .attr("class", "blankrectangle")
+        .attr("x", GLVARS.x_scale(0))
+        .attr("width", GLVARS.x_scale(GLVARS.maxPlotX))
+        .attr("y", GLVARS.y_scale(GLVARS.maxPlotY))
+        .attr("height", GLVARS.plot_height - GLVARS.y_scale(GLVARS.maxPlotY))
+        //.on("click", updateScorePosition)
+        //.on("mousemove", updateMouseTrackLine)
+        //.on("mouseout", removeMouseTrackLine)
+        .on("mousemove", showSuitableVideoDivsForCurrentMousePosition)
+    ;
+
+    createPageTicks(_svg, GLVARS.pageTimes);
+
+    createRectangles(_svg, GLVARS.allVideoSegments);
+
+    createCurves(_svg, GLVARS.curves);
+
+    createRadioButtons(_svg, GLVARS.radiobuttons);
+}
--- js/sync-visualization-main.js
+++ js/sync-visualization-main.js
@@ -35,8 +35,8 @@ $('#scoreIDs').change(function () {
     scoreId = $("#scoreIDs").val();
 
     var quality = $("#qualityFilter").val();
-    console.log("ScoreID: " + scoreId + "    Qual: " + quality);
-    _pnq.push(['loadScore', scoreId]);
+    //console.log("ScoreID: " + scoreId + "    Qual: " + quality);
+    //_pnq.push(['loadScore', scoreId]);
     loadDataForScoreID(scoreId, quality);
 });
 
@@ -50,7 +50,7 @@ $('#qualityFilter').change(function () {
 
     var scoreId = $("#scoreIDs").val();
 
-    _pnq.push(['loadScore', scoreId]);
+    //_pnq.push(['loadScore', scoreId]);
     loadDataForScoreID(scoreId, quality);
 });
 // populate the dropdown for quality selection
@@ -88,7 +88,7 @@ $.getJSON('IMSLP-YT-AlignmentQuality.json', function (json) {
 
     initialScoreId = GLVARS.sIDs[0];
 
-    _pnq.push(['loadScore', initialScoreId]);
+    //_pnq.push(['loadScore', initialScoreId]);
     loadDataForScoreID(initialScoreId, 0);
     setTimeout(function () {loadDataForScoreID(initialScoreId, 0); }, 1000);
 
@@ -124,11 +124,17 @@ function resetScoreDOM() {
     $('#plotContainer').empty();
 }
 
+function loadScore(_sID) {
+    _pnq.push(['loadScore', _sID]);
+}
+
 function loadDataForScoreID(_sID, _quality) {
     'use strict';
 
     resetScoreVariables(_sID);
     resetScoreDOM();
+
+    loadScore(_sID);
 
     var svg = createPlotSVG(), doneCount = 0, doneCountCheck1 = 0, allScoreToVideoPairsSyncData = [], vid, url, noRestrictedVideos = [];
 
@@ -281,31 +287,6 @@ function computePlotDimensions() {
     GLVARS.maxPlotY = GLVARS.numberOfVideoSegmentLevels * (CONSTANTS.SEGMENT_RECT_HEIGHT + CONSTANTS.DISTANCE_BETWEEN_SEGMENT_RECTS) + CONSTANTS.DISTANCE_BETWEEN_SEGMENT_RECTS / 2;
     GLVARS.y_scale.domain([GLVARS.minPlotY, GLVARS.maxPlotY]);
     //console.log("extremes: " + GLVARS.maxPlotX_basis + "     " + GLVARS.numberOfVideoSegmentLevels * (CONSTANTS.SEGMENT_RECT_HEIGHT+CONSTANTS.DISTANCE_BETWEEN_SEGMENT_RECTS));
-}
-
-function drawPlot(_svg) {
-    'use strict';
-
-    // add blank rectangle
-    _svg.append("rect")
-        .attr("class", "blankrectangle")
-        .attr("x", GLVARS.x_scale(0))
-        .attr("width", GLVARS.x_scale(GLVARS.maxPlotX))
-        .attr("y", GLVARS.y_scale(GLVARS.maxPlotY))
-        .attr("height", GLVARS.plot_height - GLVARS.y_scale(GLVARS.maxPlotY))
-        //.on("click", updateScorePosition)
-        //.on("mousemove", updateMouseTrackLine)
-        //.on("mouseout", removeMouseTrackLine)
-        .on("mousemove", showSuitableVideoDivsForCurrentMousePosition)
-        ;
-
-    createPageTicks(_svg, GLVARS.pageTimes);
-
-    createRectangles(_svg, GLVARS.allVideoSegments);
-
-    createCurves(_svg, GLVARS.curves);
-
-    createRadioButtons(_svg, GLVARS.radiobuttons);
 }
 
 function computePlotElements(_allScoreToVideoPairsSyncData) {
@@ -571,159 +552,6 @@ function assignSegmentYCoordinates(_arrayOfSortedRects) {
 
         }
     }
-}
-
-
-// put page numbers at appropriate times on the score time axis
-function createPageTicks(_svg, _pageTimes) {
-    'use strict';
-
-    var key, betterLabelShift;
-    for (key in _pageTimes) {
-        //console.log("page: " + key);
-        if (_pageTimes.hasOwnProperty(key)) {
-            betterLabelShift = (0 - - key) > 9 ? GLVARS.labelShift : GLVARS.labelShift / 2;
-            _svg.append("text")
-                .attr("x", GLVARS.x_scale(_pageTimes[key]) - betterLabelShift)
-                .attr("y", GLVARS.y_scale(0))
-                .attr("font-family", "sans-serif")
-                .attr("font-size", "12px")
-                .attr("fill", "grey")
-                .text(key)
-            ;
-        }
-    }
-}
-
-function createRectangles(_svg, _rects) {
-    'use strict';
-
-    _svg.selectAll(".bar")
-        .data(_rects)
-        .enter().append("rect")
-        .attr("class", "rectangle")
-        .attr("x", function (d) { return GLVARS.x_scale(d.x1); })
-        .attr("width", function (d) { return GLVARS.x_scale(d.width); })
-        .attr("y", function (d) { return GLVARS.y_scale(d.y); })
-        .attr("height", GLVARS.plot_height - GLVARS.y_scale(CONSTANTS.SEGMENT_RECT_HEIGHT))
-        //.on("click", updateVideoPositionRect)
-        .on("mouseover", enlargeVideoDivRect)
-        //.on("mouseout", resetVideoDivRect)
-        .append("svg:title")
-        .text(function (d) {return GLVARS.videoTitle[d.videoID]});
-        ;
-}
-
-function createCurves(_svg, _curves) {
-    'use strict';
-
-    // http://www.dashingd3js.com/svg-paths-and-d3js
-    // var lineData = [ { "x": 61,  "y": 0.75}, { "x": 80,  "y": 0.75},
-    //                  { "x": 55,  "y": 2.75}, { "x": 61,  "y": 2.75}];
-    var lineFunction = d3.svg.line()
-        .x(function (d) { return GLVARS.x_scale(d.x); })
-        .y(function (d) { return GLVARS.y_scale(d.y); })
-        .interpolate("basis");
-
-    _svg.selectAll(".curve")
-        .data(_curves)
-        .enter().append("path")
-        .attr("d", function (d) {return lineFunction(d.points); })
-        .attr("stroke", "blue")
-        .attr("stroke-width", 3)
-        .attr("stroke-dasharray", function (d) {return d.strokeDash; })
-        //.attr("stroke-dasharray", "0,0")
-        .attr("fill", "none")
-        .on("click", updateVideoPositionCurve)
-        .on("mouseover", enlargeVideoDivCurve)
-        //.on("mouseout", resetVideoDivCurve)
-        //;
-}
-
-function createRadioButtons(_svg, _radiobuttons) {
-    'use strict';
-
-    var i, plotDiv = document.getElementById("plotContainer"), selectVideoRB, topForRB, rbID;
-    // if loop starts from 0 then rb will be added from bottom to top and this affects control from keyboard
-    for (i = _radiobuttons.length - 1; i >= 0; i = i - 1) {
-
-        selectVideoRB = document.createElement('input');
-
-        rbID = _radiobuttons[i].videoID + "_" + _radiobuttons[i].segmentIndex + "_RB";
-        selectVideoRB.setAttribute('id', rbID);
-        //selectVideoRB.setAttribute('index', _radiobuttons[i].index);
-        selectVideoRB.setAttribute('type', 'radio');
-        selectVideoRB.setAttribute('name', 'selectVideoGroupRB');
-        selectVideoRB.setAttribute('class', 'videoRB');
-        selectVideoRB.setAttribute('onclick', 'rbClickHandler(this)');
-
-        topForRB = GLVARS.y_scale(_radiobuttons[i].y) + GLVARS.plot_margin.top + GLVARS.plot_margin.bottom;
-        selectVideoRB.style.top = topForRB + "px";
-
-        plotDiv.appendChild(selectVideoRB);
-
-        if (!GLVARS.rbIndex.hasOwnProperty((rbID))) {
-            GLVARS.rbIndex[rbID] = i;
-        }
-
-//        d3.select("g").append("line")
-//            .attr("class", "videoTopLine")
-//            .attr("x1", GLVARS.x_scale(0))
-//            .attr("y1", GLVARS.y_scale(_radiobuttons[i].y))
-//            .attr("x2", GLVARS.x_scale(GLVARS.maxPlotX))
-//            .attr("y2", GLVARS.y_scale(_radiobuttons[i].y))
-//            .attr("stroke-width", 1)
-//            .attr("stroke", "lightgrey")
-//            .attr("pointer-events", "none");
-    }
-
-}
-
-function createPlotSVG() {
-    'use strict';
-
-    var svg_basis = d3.select("#plotContainer").append("svg")
-        .attr("width", GLVARS.plot_width + GLVARS.plot_margin.left + GLVARS.plot_margin.right)
-        .attr("height", GLVARS.plot_height + GLVARS.plot_margin.top + GLVARS.plot_margin.bottom)
-        .append("g")
-        .attr("transform", "translate(" + GLVARS.plot_margin.left + "," + GLVARS.plot_margin.top + ")")
-
-        .on("click", updateScorePosition)
-        .on("mousemove", updateMouseTrackLine)
-        //.on("mousemove", showSuitableVideoDivsForCurrentMousePosition)
-        //.on("mousemove", handleMouseMoveEvent)
-        .on("mouseout", removeMouseTrackLine)
-        ;
-
-    svg_basis.append("g")
-        .attr("class", "x axis")
-        .attr("transform", "translate(0," + GLVARS.plot_height + ")")
-        .call(GLVARS.xAxis);
-
-    // don't show the ticks of x-axis
-    GLVARS.xAxis.tickFormat(function (d) { return ''; });
-
-    //drawYAxis(svg_basis);
-
-    return svg_basis;
-}
-
-
-function drawYAxis(svg_basis) {
-    'use strict';
-
-    var yAxis = d3.svg.axis()
-        .scale(GLVARS.y_scale)
-        .orient("left");
-    svg_basis.append("g")
-        .attr("class", "y axis")
-        .call(yAxis)
-        .append("text")
-        .attr("transform", "rotate(-90)")
-        .attr("y", 6)
-        .attr("dy", ".71em")
-        .style("text-anchor", "end")
-        .text("Frequency");
 }
 
 function rbClickHandler(d) {
